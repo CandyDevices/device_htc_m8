f35ab59e61e3f9dac748a046429919a1d015ac35
rc420head <dennislebhardt@yahoo.com>
2015-03-14 16:57:02 -0500

    use linaro 4.10
    Conflicts:
    	BoardConfig.mk
    
    Conflicts:
    	BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -62,7 +62,16 @@ BOARD_MKBOOTIMG_ARGS := --kernel_offset 0x00008000 --ramdisk_offset 0x02008000 -
 BOARD_CUSTOM_BOOTIMG_MK := device/htc/m8/mkbootimg.mk
 TARGET_KERNEL_CONFIG := cm_m8_defconfig
 TARGET_KERNEL_SOURCE := kernel/htc/msm8974
-TARGET_KERNEL_CUSTOM_TOOLCHAIN := prebuilts/gcc/linux-x86/arm/arm-eabi-4.9/bin
+KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/Linaro-4.10/bin
+KERNEL_TOOLCHAIN_PREFIX := arm-eabi-
+
+# Rom Optimization
+USE_GRAPHITE := true
+USE_FSTRICT_FLAGS := true
+FSTRICT_ALIASING_WARNING_LEVEL := 3
+USE_BINARY_FLAGS := true
+USE_EXTRA_CLANG_FLAGS := true
+USE_LTO := true
 
 # QCOM hardware
 BOARD_USES_QCOM_HARDWARE := true
