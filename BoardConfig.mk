e31da67779df5eb543a97cbf20aaf30e98de029a
rc420head <dennislebhardt@yahoo.com>
2015-05-15 23:21:28 -0500

    Rom Optimization

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -64,6 +64,14 @@ TARGET_KERNEL_SOURCE := kernel/htc/msm8974
 KERNEL_TOOLCHAIN := $(ANDROID_BUILD_TOP)/prebuilts/gcc/$(HOST_OS)-x86/arm/arm-candy-linux-gnueabi/bin
 KERNEL_TOOLCHAIN_PREFIX := arm-CANDY-linux-gnueabi-
 
+# Rom Optimization
+USE_GRAPHITE := true
+USE_FSTRICT_FLAGS := true
+FSTRICT_ALIASING_WARNING_LEVEL := 3
+USE_BINARY_FLAGS := true
+USE_EXTRA_CLANG_FLAGS := true
+USE_LTO := true
+
 # QCOM hardware
 BOARD_USES_QCOM_HARDWARE := true
 
